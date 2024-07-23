8c4c28adc057cd4a6ebd301332f71930b8943c0d
Richard <richardmusime6@gmail.com>
2024-07-23 12:12:57 +0200

    Added the UI of the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <>
+     <div className="container">
+            <img src="https://picsum.photos/640/360" />
+            <button>Click me</button>
+        </div>
+   </>
+  )
+}
+
+export default App
