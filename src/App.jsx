b666115dafe141074804160e291f9cd4cfed2643
Richard <richardmusime6@gmail.com>
2024-07-23 15:14:15 +0200

    Added a counter app made in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import image1 from './assets/imag1.jpg'
+import image2 from './assets/image2.jpg'
+
 
 function App() {
+//   // const [image, setImage] = useState("")
+//   // const [toggle, setToggle] = useState(true)
+//   // function handleClick() {
+//   //   // Set the image depending on the value of toggle
+//   //   if (toggle) {
+//   //     setImage(image1)
+//   //   } else {
+//   //     setImage(image2)
+//   //   }
+    
+//   //   // After each click, update the value of toggle
+//   //   setToggle(!toggle)
+
+//   }
+      
+       const [increase,setIncrease] = useState(0)
+       function handleAdd() {
+        setIncrease(increase + 1)
+       }
+       function handledecrease() {
+        setIncrease(increase - 1)
+       }
+
   return (
    <>
      <div className="container">
-            <img src="https://picsum.photos/640/360" />
-            <button>Click me</button>
+            {/* <img src={image}/> */}
+            <h1>{increase}</h1>
+            <div className='flex gap-6'>
+            <button onClick={handleAdd}>Increase me</button>
+            <button onClick={handledecrease}>Decrease me</button>
+
+            </div>
         </div>
    </>
   )
